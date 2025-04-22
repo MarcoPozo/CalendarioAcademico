4325caf472fe27a4238889b258bd9d85ec0bb077
MarcoPozo <marco10011111@gmail.com>
2025-04-22 16:42:10 -0500

    Se movio validacion de SESSION_SECRET

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import "dotenv/config";
 import express from "express";
+
+if (!process.env.SESSION_SECRET) {
+  throw new Error("❌ SESSION_SECRET no está definido. Revisa las variables en Railway.");
+}
+
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import indexRouter from "./routes/router.js";
