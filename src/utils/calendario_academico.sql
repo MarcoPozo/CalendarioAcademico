d0dedc542634e3c93ffb20eeda7ee7765d0dbf08
MarcoPozo <marco10011111@gmail.com>
2025-04-24 13:32:50 -0500

    Se modifico sql para importar

--- src/utils/calendario_academico.sql
+++ src/utils/calendario_academico.sql
@@ -1,7 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 4.8.0.1
 -- https://www.phpmyadmin.net/
---
 -- Servidor: 127.0.0.1
 -- Tiempo de generación: 24-04-2025 a las 19:28:02
 -- Versión del servidor: 10.1.32-MariaDB
@@ -12,21 +11,8 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `calendario_academico`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `admin_users`
---
+DROP TABLE IF EXISTS `eventos`;
+DROP TABLE IF EXISTS `admin_users`;
 
 CREATE TABLE `admin_users` (
   `id` int(11) NOT NULL,
@@ -35,18 +21,8 @@ CREATE TABLE `admin_users` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Volcado de datos para la tabla `admin_users`
---
-
 INSERT INTO `admin_users` (`id`, `username`, `password`, `created_at`) VALUES
 (2, 'admin', '$2b$10$7FydxVg8gi928fFFd8tW1.dqaqAvaU/hJ/dH/Nn6Tq6XK7eCQZd1y', '2025-04-14 21:15:14');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `eventos`
---
 
 CREATE TABLE `eventos` (
   `id` int(11) NOT NULL,
@@ -57,45 +33,18 @@ CREATE TABLE `eventos` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Volcado de datos para la tabla `eventos`
---
-
-INSERT INTO `eventos` (`id`, `fecha`, `titulo`, `color`, `created_at`, `updated_at`) VALUES
-(29, '2025-04-24', 'Clases', '#3a8cd9', '2025-04-24 17:13:00', '2025-04-24 17:13:00'),
-(30, '2025-04-21', 'Clases', '#eae31f', '2025-04-24 17:15:36', '2025-04-24 17:15:36');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `admin_users`
---
 ALTER TABLE `admin_users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
---
--- Indices de la tabla `eventos`
---
 ALTER TABLE `eventos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `idx_fecha` (`fecha`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `admin_users`
---
 ALTER TABLE `admin_users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT de la tabla `eventos`
---
 ALTER TABLE `eventos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
 COMMIT;
